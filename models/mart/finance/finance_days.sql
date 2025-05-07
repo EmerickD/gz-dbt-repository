34333e009a24e70d6fbe479b63ace46ecb604c35
Emerick Dumas <emerickdumas@gmail.com>
2025-05-07 15:07:52 +0000

    v4

--- models/mart/finance/finance_days.sql
+++ models/mart/finance/finance_days.sql
@@ -1,5 +1,7 @@
-Select 
-date_date,
+{{ config(materialized='table') }}
+
+  Select 
+    date_date,
     COUNT(DISTINCT orders_id) AS total_transactions,
     ROUND(SUM(total_revenue), 2) AS revenue,
     ROUND(SUM(total_revenue) / COUNT(DISTINCT orders_id), 2) AS average_basket,
@@ -9,6 +11,6 @@ date_date,
     ROUND(SUM(shipping_fee), 2) AS total_shipping_fee,
     ROUND(SUM(logcost), 2) AS total_logcost,
     SUM(total_quantity) AS total_quantity
-    FROM {{ref('int_orders_operational')}}
-    GROUP BY date_date
-    ORDER BY date_date
+  FROM {{ref('int_orders_operational')}}
+  GROUP BY date_date
+  ORDER BY date_date
